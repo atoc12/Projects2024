9d395f7e8b543d59357ae698e07bd2a8d5d25caa
unknown <octaviosdf1@gmail.com>
2024-04-24 21:58:03 -0300

    Drag and Drop is implement in the app To do

--- to do/main.js
+++ to do/main.js
@@ -1,57 +1,56 @@
-
-
 const Task = []; // Guarda las tareas
-
-
+var elementMove = null;
 const libreta={ // Diccionario que guarda los elementos HTML en los que se va visualizar las tareas
     task:document.getElementById("task"),
     progress:document.getElementById("progress"),
     end:document.getElementById("end"),
 }
 
+
 function dropBoxAction () {
     let status = this.getAttribute("data-open");
     this.setAttribute("data-open",status == "close" ? "open" : "close");
     
 }
+function createComponent (form){
+    const component = document.createElement("div");
+    component.setAttribute("draggable","true");
+    component.setAttribute("class","card-task");
+    component.setAttribute("data-open","close");
+    component.innerHTML=`
+    <div>
+        <h1>
+            ${form.tittle}
+        </h1>
+    </div>
+    <div>
+        <p>${form.description}</p>
+    </div>
+`;
 
-const component = document.createElement("div");
-component.setAttribute("class","card-task");
-component.setAttribute("data-open","close");
+    return component;
+}
+function cloneEventsComponent (element){
+    element.addEventListener("click",dropBoxAction);
+    element.addEventListener("dragstart",(e)=>elementMove = element);
+    return element;
+}
+
 
 
 
 document.getElementById("form-task").addEventListener("submit",(e)=>{ // Escucha a qué se haya enviado los datos
     try{
-
         e.preventDefault();// Elimina los eventos por defecto
-
         let form ={ // Guarda los valores del formulario en un diccionario
             tittle:e.target["titulo-form"].value,
             description:e.target["description-form"].value,
             date:Date.now(),
         }
 
-        let childClone = component.cloneNode(true);
-
-        childClone.addEventListener("click",dropBoxAction);
-
-        childClone.innerHTML=`
-
-            <div>
-                <h1>
-                    ${form.tittle}
-                </h1>
-                
-            </div>
-
-            <div>
-                
-                <p>${form.description}</p>
-            </div>
-
-        `;
-
+        let childClone = createComponent(form);
+        childClone= cloneEventsComponent(childClone);
+        
         libreta.task.append(childClone);
 
 
@@ -61,3 +60,14 @@ document.getElementById("form-task").addEventListener("submit",(e)=>{ // Escucha
 })
 
 
+for (let element in libreta) {
+    libreta[element].addEventListener("dragover", (e) => e.preventDefault());
+    libreta[element].addEventListener("drop", (e) => {
+        if (elementMove) {
+            let cloneElement = cloneEventsComponent(elementMove.cloneNode(true));
+            e.target.append(cloneElement);
+            elementMove.parentNode.removeChild(elementMove);
+            elementMove = null;
+        }
+    });
+}
