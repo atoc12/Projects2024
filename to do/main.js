7a5d1d918fb55fc7b120307a12d2ecd8bc71682a
unknown <octaviosdf1@gmail.com>
2024-04-25 12:13:44 -0300

    Repair bugs and implement count of the task

--- to do/main.js
+++ to do/main.js
@@ -1,45 +1,46 @@
-const Task = []; // Guarda las tareas
+const TASK = []; // Guarda las tareas
 var elementMove = null;
 const libreta={ // Diccionario que guarda los elementos HTML en los que se va visualizar las tareas
     task:document.getElementById("task"),
     progress:document.getElementById("progress"),
     end:document.getElementById("end"),
+};
+
+function countUpdate (){
+    document.getElementById("countTask").innerText=TASK.length;
 }
-
-
-function dropBoxAction () {
+function dropBoxAction () { // funcion que permite abrir el DropBox
     let status = this.getAttribute("data-open");
-    this.setAttribute("data-open",status == "close" ? "open" : "close");
-    
-}
-function createComponent (form){
+    this.setAttribute("data-open",status == "close" ? "open" : "close");   
+};
+function createComponent (form){ // Crea un componente
     const component = document.createElement("div");
     component.setAttribute("draggable","true");
     component.setAttribute("class","card-task");
     component.setAttribute("data-open","close");
-    component.innerHTML=`
+    component.innerHTML=
+    `
     <div>
         <h1>
             ${form.tittle}
         </h1>
+
     </div>
-    <div>
+
+    <div class="card-task-container">
         <p>${form.description}</p>
     </div>
-`;
-
+    `;
+    TASK.push({...form,body:component});
+    countUpdate();
     return component;
-}
-function cloneEventsComponent (element){
+};
+function cloneEventsComponent (element){// retorna el elemento copiado con eventos predefinidos
     element.addEventListener("click",dropBoxAction);
     element.addEventListener("dragstart",(e)=>elementMove = element);
     return element;
-}
-
-
-
-
-document.getElementById("form-task").addEventListener("submit",(e)=>{ // Escucha a qué se haya enviado los datos
+};
+function handleSubmit (e){//Ejecución del formulario
     try{
         e.preventDefault();// Elimina los eventos por defecto
         let form ={ // Guarda los valores del formulario en un diccionario
@@ -47,27 +48,36 @@ document.getElementById("form-task").addEventListener("submit",(e)=>{ // Escucha
             description:e.target["description-form"].value,
             date:Date.now(),
         }
-
-        let childClone = createComponent(form);
-        childClone= cloneEventsComponent(childClone);
-        
-        libreta.task.append(childClone);
-
-
+        if(form.tittle.length > 0 && form.tittle != " " ){
+            let childClone = createComponent(form);
+            childClone= cloneEventsComponent(childClone);
+            libreta.task.append(childClone);
+        }else{
+            alert("Es necesario utilizar un titúlo");
+        }
     }catch(Err){
         alert(Err.message);
     }
-})
-
-
-for (let element in libreta) {
-    libreta[element].addEventListener("dragover", (e) => e.preventDefault());
-    libreta[element].addEventListener("drop", (e) => {
-        if (elementMove) {
-            let cloneElement = cloneEventsComponent(elementMove.cloneNode(true));
-            e.target.append(cloneElement);
-            elementMove.parentNode.removeChild(elementMove);
-            elementMove = null;
-        }
-    });
+};
+function dropTask (e,element){
+    if (e.target == libreta[element] ) {
+        let cloneElement = cloneEventsComponent(elementMove.cloneNode(true));
+        e.target.append(cloneElement);
+        elementMove.parentNode.removeChild(elementMove);
+        elementMove = null;
+    }
 }
+function removeTask (element){
+    element.parentNode.removeChild(element);
+    
+}
+
+document.getElementById("form-task").addEventListener("submit",handleSubmit);
+for (let element in libreta) {
+    libreta[element].addEventListener("dragover", (e) => {
+        e.preventDefault();
+    });
+
+    libreta[element].addEventListener("drop",e=>dropTask(e,element));
+};
+
